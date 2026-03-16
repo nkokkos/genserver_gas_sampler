962c81040a9e3e738fc24905d4ff4d2ba377ce78
Nick K. <nkokkos@gmail.com>
2026-03-16 13:28:30 +0200

    Define SampleSensor module for gas sensor
    Added a module definition for SampleSensor with documentation.

--- sampler.ex
+++ sampler.ex
@@ -1,1 +1,8 @@
-Code Starts here
+# Code Starts here
+defmodule SampleSensor do
+  @moduledoc """
+  GenServer for a Gas Sensor via ADS1115 ADC.
+  Samples 7 times evenly spread over 5 seconds.
+  Applies median filter and saves result to state.
+  """
+end
