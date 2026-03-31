7350f501e361aa0cb8131d13a1774c58d87096da
Nick Kokkos <nkokkos@gmail.com>
2026-03-31 11:45:04 +0300

    added shoehorn dependencies

--- sampler/config/config.exs
+++ sampler/config/config.exs
@@ -3,16 +3,28 @@
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
+
+# Import the Config module that provides functions like config and import_config
 import Config
 
 # Enable the Nerves integration with Mix
+# This is a build time command which starts the nerves bootstrap 
+# application on the host machine(laptopm/linux system etc)
+# It allows Mix to understand Nerves-specific commands and helps 
+# manage the cross-compilation needed to build code for 
+# a Raspberry Pi Zero from a non-Pi computer.
 Application.start(:nerves_bootstrap)
 
+# It stores the current build target into the application's environment
+# For example, you set target to be rpi0 or something else
 config :sampler, target: Mix.target()
 
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
-
+# It points to a folder in the current project (sampler/rootfs_overlay)
+# What it does: Anything we put in that folder, like a custom iex.exs or 
+# some other script, will be copied directly to the rasberry pi's read 
+# only system. Basically, this is a way to "inject" files to the Linux OS
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
@@ -23,9 +35,27 @@ config :nerves, source_date_epoch: "1577975236"
 # Use Ringlogger as the logger backend and remove :console.
 # See https://hexdocs.pm/ring_logger/readme.html for more information on
 # configuring ring_logger.
-
+# It replaces the standard Elixir console logger with RingLogger
+# What it does: SDCards have a limited number of "writes" before they die.
+# Ringlogger logs in a circular buffer in RAM rather than writing them to the
+# SD card. This saves the hardware from wearing out and allows viewing logs
+# by typing: RingLogger.new
 config :logger, backends: [RingLogger]
 
+
+# use shoehorn to handle otp application failures and load
+# primary application components
+# https://github.com/nerves-project/shoehorn
+
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_pack],
+  app:  :sampler
+
+# Picks ups configuration based on the host
+# What it does: host.exs contains settings for our host(laptop). For 
+# example using a fake Mock sensor
+# Target.exs contains settings for the PI zero like wifi credentials, I2C bus
+# addresses for the devices we will use
 if Mix.target() == :host do
   import_config "host.exs"
 else
